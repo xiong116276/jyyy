e8e6736535c4781241e140ee562e6a3199b3057c
xiong116276 <1162767656@qq.com>
2018-06-14 15:39:38 +0800

    hack-ie9.js

--- PC/b2bPC/js/hack-ie8.js
+++ PC/b2bPC/js/hack-ie8.js
@@ -8,6 +8,8 @@ $(".goods-list-container-new .goods-show .show-table .goods-item:nth-child(5n)")
   $(this).css("margin-right",0);
 });
 
+//input placeholder
+
 //添加提示
 var tips = function(){
   alert('你所用的浏览器版本过低，为了更好的体验，请升级或更换浏览器！');
