cbc3e8a86a410d5e14fcfca56dc25b93bc363619
xiong116276 <1162767656@qq.com>
2018-06-14 15:56:14 +0800

    branch

--- web/b2b-0.2/js/rem.js
+++
@@ -1,9 +0,0 @@
-function getRem(pwidth,prem){
-  var html = document.documentElement;
-  var oWidth = window.innerWidth<pwidth? window.innerWidth:pwidth;
-  html.setAttribute("style","font-size: "+oWidth/pwidth*prem+"px !important");
-}
-getRem(750,100);
-window.onresize = function () {
-  getRem(750,100);
-};
